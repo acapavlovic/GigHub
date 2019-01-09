a019aba9f7775157797fcd9e5ff4662c825c1816
Aleksandar <Aleksandar@ACA>
2019-01-09 10:42:49 +0100

    Create a gig model.

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gig { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
