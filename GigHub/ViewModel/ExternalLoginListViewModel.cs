9cb5b06bf5ba7cca49875d3fbfd0c6ec86361e27
Aleksandar <Aleksandar@ACA>
2019-01-10 00:58:03 +0100

    Add name to artist.

---
+++ GigHub/ViewModel/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GigHub.ViewModel
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ GigHub/ViewModel/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModel
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
